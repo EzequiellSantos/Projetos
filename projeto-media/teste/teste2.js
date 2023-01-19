a2ccb89980ad0109f5e6ec7f73dfe6c5576324cb
EzequiellSantos <111365548+EzequiellSantos@users.noreply.github.com>
2023-01-19 10:57:22 -0300

    updte teste

--- projeto-media/teste/teste2.js
+++ projeto-media/teste/teste2.js
@@ -1,7 +1,26 @@
 var input = document.getElementById('input')
 var ver = document.getElementById('ver')
+var res = document.getElementById('res')
+var saida = document.getElementById('saida')
 ver.addEventListener('click', clicou)
 
 function clicou(){
+    /* 
+
+    var valor = (Number(input.value) * 2) / 2
+    res.innerText = `${valor}` 
     
+    */
+    var notas = 0
+    for(var mat = 1 ; mat <= 2 ; mat ++){
+
+        /* notas += saida.innerHTML = '<input type="number" inputmode="decimal">' */
+
+        notas += Number(prompt('digite a nota'))
+    }
+
+    var total = notas / 2
+    res.innerText = `${total}`
+
+
 }
