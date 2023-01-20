c43a1c03bdb5654957d62d677ae46f625ad2ffaf
EzequiellSantos <111365548+EzequiellSantos@users.noreply.github.com>
2023-01-20 11:00:44 -0300

    Updates projeto-media

--- projeto-media/teste/scripttest.js
+++ projeto-media/teste/scripttest.js
@@ -1,3 +1,5 @@
+var  materias = window.prompt('Quantas matérias tem o seu curso?')
+
 var animateButton = function (e) {
     e.preventDefault;
     //reset animation
@@ -16,14 +18,30 @@ for (var i = 0; i < bubls.length; i++) {
 }
 
 ///////////////////////////
-
+//var body = document.getElementsByTagName('body')
 var ver = document.getElementById('ver')
 var res = document.getElementById('res')
 var input = document.getElementById('input')
+
 ver.addEventListener('click', clicou)
+//body.addEventListener('load', carregou)
+
+//function carregou(){
+    
+    var p1 = document.getElementsByTagName('p')[0]
+    if(materias > 1){
+    p1.innerHTML = `<p class="p1">Para Cursos Com <span class="quant">${materias}</span> Matérias</p>`}else if(materias == 1){
+        p1.innerHTML = `<p class="p1">Para Cursos Com <span class="quant">${materias}</span> Matéria</p>`
+    }else{
+        p1.innerHTML = '<p>[ERROR_]</p>'
+    }
+//}
+
+
+
 
 const materia = [
-    'AI:',
+    /* 'AI:',
     'Bio:',
     'Ed.Fís:',
     'Filo:',
@@ -38,7 +56,27 @@ const materia = [
     'Quim:',
     'RET:',
     'Socio:',
-    'Formação Cidadã Undefined_:'
+    'Formação Cidadã Undefined_:' */
+    '1 :',
+    '2 :',
+    '3 :',
+    '4 :',
+    '5 :',
+    '6 :',
+    '7 :',
+    '8 :',
+    '9 :',
+    '10 :',
+    '11 :',
+    '12 :',
+    '13 :',
+    '14 :',
+    '15 :',
+    '16 :',
+    '17 :',
+    '18 :',
+    '19 :',
+    '20 :'
     
 ]
 
@@ -48,34 +86,19 @@ function clicou(){
 
     
     var notas = 0
-    for ( var Mat = 1  ; Mat <=16 ; Mat++){
-       res.innerHTML ='<input type="number">'
-       input.focus() 
-       notas = input
+    for ( var Mat = 1  ; Mat <= materias; Mat++){
+        notas += Number(prompt(`Qual a nota de ${materia[Mat - 1]}`))
     }
 
-    var total = notas / 16
+    var total = notas / materias
+    var totalfrm = parseFloat(total.toFixed(2)) 
 
-
-
-
-
-/////////////////////////////////////////////
-
-
-    /* res.innerText = `${total}` */
-
-
-
-
-
-    /* var totalfrm = parseFloat(total.toFixed(2)) */
-    /* if(total < 6){
+    if(total < 6){
         res.innerHTML += `<p> Sua média final é: <mark class='nobix'>${totalfrm}</mark></p>`
     }else if(total < 8.5){
         res.innerHTML += `<p> Sua média final é: <mark class="intr">${totalfrm}</mark></p>`
     }else{
         res.innerHTML += `<p> Sua média final é: <mark class="bix">${totalfrm}</mark></p>`
-    } */
+    } 
      
 }
