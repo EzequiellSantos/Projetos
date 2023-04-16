ab456dcb194355659c67c0ad723cd9f853e7100b
EzequiellSantos <111365548+EzequiellSantos@users.noreply.github.com>
2023-04-16 16:44:30 -0300

    Atualização script.js Identação

--- projeto-media/teste/script.js
+++ projeto-media/teste/script.js
@@ -1,73 +1,81 @@
-          const noteInput = document.querySelector("#noteInput");
-          const addNoteButton = document.querySelector("#addNote");
-          const notesList = document.querySelector("#notes");
-          const averageElement = document.querySelector("#average");
+const noteInput = document.querySelector("#noteInput");
+const addNoteButton = document.querySelector("#addNote");
+const notesList = document.querySelector("#notes");
+const averageElement = document.querySelector("#average");
 
-          
-          let notes = [];
-           
 
-          addNoteButton.addEventListener("click", () => {
-           
-            if (noteInput.value.length == '') {
+let notes = [];
+  
 
-              alert('Digite uma Nota!')   
+addNoteButton.addEventListener("click", () => {
+  
+  if (noteInput.value.length == '') {
 
-            }else{
+    alert('Digite uma Nota!')   
 
-              const note = parseFloat(noteInput.value);
-              if (!isNaN(note)) {
-               notes.push(note);
-               noteInput.value = "";
-               renderNotes();
-              }
-            }
-          });
+  }else{
+
+    const note = parseFloat(noteInput.value);
+    if (!isNaN(note)) {
+      notes.push(note);
+      noteInput.value = "";
+      renderNotes();
+    }
+  }
+});
+
+function renderNotes() {
+  notesList.innerHTML = "";
+  noteInput.focus()
+
+
+  for (let i = 0; i < notes.length; i++) {
+    const note = notes[i];
+    const noteElement = document.createElement("li");
+    //criando a lista de todas as notas
+    noteElement.innerText = note;
+
+    const editButton = document.createElement("button");
+    editButton.setAttribute('class', 'edit')
+    editButton.innerHTML ='<img src="editar.png">' ;
+    //criação do botão de editas
     
-          function renderNotes() {
-            notesList.innerHTML = "";
-            noteInput.focus()
-          
-          
-            for (let i = 0; i < notes.length; i++) {
-              const note = notes[i];
-              const noteElement = document.createElement("li");
-              noteElement.innerText = note;
-    
-              const editButton = document.createElement("button");
-              editButton.setAttribute('class', 'edit')
-              editButton.innerHTML ='<img src="editar.png">' ;
-              
-              editButton.addEventListener("click", () => {
-                noteInput.value = note;
-                notes.splice(i, 1);
-                renderNotes();
-              });
-              noteElement.appendChild(editButton);
-    
-              const deleteButton = document.createElement("button");
-              deleteButton.setAttribute('class', 'delet')
-              deleteButton.innerHTML= '<img src="excluir.png">';
-              deleteButton.addEventListener("click", () => {
-                notes.splice(i, 1);
-                renderNotes();
-              });
-              noteElement.appendChild(deleteButton);
-    
-              notesList.appendChild(noteElement);
-            }
-            updateAverage();
-          }
-          
-          function updateAverage() {
-            let sum = 0;
-            for (const note of notes) {
-              sum += note;
-            }
-            const average = sum / notes.length;
-            averageElement.innerText = average.toFixed(2);
-          }
-          
-         function reload(){
-          window.location.reload(true)
-         }
+    editButton.addEventListener("click", () => {
+      noteInput.value = note;
+      notes.splice(i, 1);
+      renderNotes();
+      //atualização das notas
+    });
+    noteElement.appendChild(editButton);
+
+    const deleteButton = document.createElement("button");
+    deleteButton.setAttribute('class', 'delet')
+    deleteButton.innerHTML= '<img src="excluir.png">';
+    //criação do botão de deletar
+
+    deleteButton.addEventListener("click", () => {
+      notes.splice(i, 1);
+      renderNotes();
+      //atualização do delete das notas
+    });
+    noteElement.appendChild(deleteButton);
+
+    notesList.appendChild(noteElement);
+  }
+  updateAverage();
+}
+
+function updateAverage() {
+  let sum = 0;
+  for (const note of notes) {
+    sum += note;
+  }
+  const average = sum / notes.length;
+  averageElement.innerText = average.toFixed(2);
+  //calculaando a média da todas as notas presentes
+}
+
+function reload(){
+window.location.reload(true)
+//botão limpar
+}
