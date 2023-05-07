55105d3b49049d2b2c4a07f63250ddfd90a588db
EzequiellSantos <111365548+EzequiellSantos@users.noreply.github.com>
2023-05-07 12:22:49 -0300

    Update in Menu

--- projeto-portfolio/script.js
+++ projeto-portfolio/script.js
@@ -20,14 +20,13 @@ class MobileNavbar {
 
     handleClick() {
         this.navList.classList.toggle(this.activeClass)
-        /* this.mobileMenu.classList.toggle(this.desactiveClass) */
         this.animateLinks()
     }
 
     addClickEvent() {
         this.mobileMenu.addEventListener("click", this.handleClick)
     }
-    
+
     init() {
         if(this.mobileMenu) {
             this.addClickEvent()
@@ -58,45 +57,54 @@ let lineThree = document.getElementsByTagName('div')[3]
 let navMenu = document.getElementById('menu')
 let ulMenu = document.getElementsByTagName('ul')[0]
 
-burgerMenu.addEventListener('click', clicou)
-navMenu.addEventListener('mousedown', fora)
+burgerMenu.addEventListener('mouseover', clicou)
+burgerMenu.addEventListener('', none)
+navMenu.addEventListener('mouseout', fora)
 
 
-function clicou(){
+/* function clicou(){
 
     lineOne.style.transform = 'rotate(-45deg) translate(-8px, 8px)'
     lineTwo.style.opacity = '0'
     lineThree.style.transform = 'rotate(45deg) translate(-5px, -7px)'
+
     ulMenu.style.position = 'fixed'
-    burgerMenu.style.position = 'fixed'
+    burgerMenu.style.position = 'fixed' 
 }
 
-document.addEventListener('click', function(evento) {
-    const clicadoDentroDoElemento = burgerMenu.contains(evento.target);  
-    if (!clicadoDentroDoElemento) {
+function fora(){
+        ulMenu.style.position = 'absolute'
+        burgerMenu.style.position = 'absolute'
+        lineTwo.style.opacity = '1'
+    }
+
+
+function clickOut(clickfora){
+    if(clickfora == false) {
+        lineOne.style.transform = 'rotate(45deg) translate(-8px, 8px)'
+        lineTwo.style.opacity = '0'
+        lineThree.style.transform = 'rotate(45deg) translate(-5px, -7px)'
+
+        ulMenu.style.position = 'absolute'
+        burgerMenu.style.position = 'absolute'
+    } else {
+        
+    }
+    clickOut(clicou)
+}  */ 
+
+/* if (!clicadoDentroDoElemento) 
         lineOne.style.transform = 'none'
         lineTwo.style.opacity = '1'
-        lineThree.style.transform = 'none'
-    }
-});
+        lineThree.style.transform = 'none' */
 
-/* function fora(){
-    lineOne.style.transform = 'none'
-    lineTwo.style.opacity = '1'
-    lineThree.style.transform = 'none'
-    ulMenu.style.transform = 'translateX(200%);'
-} */
+        function clicou(){
+        
+            ulMenu.style.position = 'fixed' 
+           /*  burgerMenu.style.position = 'fixed'  */
+        }
+        
 
-
-/* burgerMenu.addEventListener('click', function() {
-    navMenu.classList.toggle('menu-visivel')
-})
-
-document.addEventListener('click', function(evento){
-    if(!navMenu.contains(evento.target) && !burgerMenu.contains(evento.target)){
-        menu.classList.remove('menu-visivel')
-    }
-}) */
 
 
 
