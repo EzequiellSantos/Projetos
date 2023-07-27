ec8af606a8ab1d30ed22f4d17412c2b35d75c61f
Ezequiel Santos <111365548+EzequiellSantos@users.noreply.github.com>
2023-07-27 16:52:02 -0300

    Update Math | Fix :bug:

--- projeto-portfolio/script.js
+++ projeto-portfolio/script.js
@@ -1,3 +1,39 @@
+window.addEventListener('scroll', function() {/* section fixada no topo */
+  var section = document.querySelector('.fixed-section');
+  var sectionOffset = section.offsetTop;
+  var scrollPosition = window.scrollY;
+
+  if (scrollPosition > sectionOffset) {
+      section.classList.add('fixed');
+  } else {
+      section.classList.remove('fixed');
+  }
+});
+
+/* Configurações Menu */
+var imgMenu = document.getElementById('img-menu');
+var imgIcon = document.getElementById('img-icon');
+var menu = document.getElementById('menu');
+
+imgMenu.addEventListener('click', function() {
+  imgIcon.style.display = 'block'
+  imgIcon.style.top = '30px'
+  menu.classList.add('actived')
+  menu.style.transition = 'none'
+}) 
+
+document.addEventListener('click', function(evento) {
+  const clicadoDentroDoElemento = menu.contains(evento.target)
+
+  if(!clicadoDentroDoElemento){
+    imgIcon.style.display = 'none'
+    menu.classList.remove('actived')
+    menu.style.transition = 'all 0.6s ease-in-out'
+  }
+})
+
+////////////////////////////////////////////
+
 const card = document.querySelector('#cards');
 
 card.addEventListener('mousemove', handleMouseMove);
