64a3c3cd8dbb4ecdbbdcc7713764495043cf8132
Ezequiel Santos <111365548+EzequiellSantos@users.noreply.github.com>
2023-08-04 21:51:28 -0300

    Update Menu Math | Feat

--- projeto-portfolio/script.js
+++ projeto-portfolio/script.js
@@ -12,17 +12,38 @@ window.addEventListener('scroll', function() {/* section fixada no topo */
 
 /* Configurações Menu */
 var imgMenu = document.getElementById('img-menu');
-var imgIcon = document.getElementById('img-icon');
-var menu = document.getElementById('menu');
+var linkIcon = document.getElementById('link-menu');
+var imgIcon = document.getElementById('casa-icon');
 
-imgMenu.addEventListener('click', function() {
-  imgIcon.style.display = 'block'
-  imgIcon.style.top = '30px'
-  menu.classList.add('actived')
-  menu.style.transition = 'none'
+var menu = document.getElementById('menu');
+var allSetas = document.querySelector('.setas');
+var setaOn = document.getElementById('seta01');
+var setaTw = document.getElementById('seta02');
+var setatr = document.getElementById('seta03');
+
+
+
+menu.addEventListener('click', function() {
+
+  if(linkIcon.classList.contains('mostrar')){
+
+    menu.style.transition = 'all 0.6s ease-in-out' 
+    linkIcon.classList.remove('mostrar')
+    imgIcon.classList.remove('mostrar')
+    menu.classList.remove('actived')
+
+  }else{
+
+    linkIcon.classList.add('mostrar')
+    menu.classList.add('actived')
+    menu.style.transition = 'all 0.6s ease-in-out'
+    setTimeout( function() {
+      imgIcon.classList.add('mostrar')
+    }, 400)
+  }
 }) 
 
-document.addEventListener('click', function(evento) {
+/* document.addEventListener('click', function(evento) {
   const clicadoDentroDoElemento = menu.contains(evento.target)
 
   if(!clicadoDentroDoElemento){
@@ -31,7 +52,7 @@ document.addEventListener('click', function(evento) {
     menu.style.transition = 'all 0.6s ease-in-out'
   }
 })
-
+ */
 ////////////////////////////////////////////
 
 const card = document.querySelector('#cards');
