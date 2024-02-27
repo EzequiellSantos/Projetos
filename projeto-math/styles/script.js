710d6dd26bd4601db1cb83fd8aa144b76b49acb0
Ezequiel Santos <111365548+EzequiellSantos@users.noreply.github.com>
2024-02-27 15:04:01 -0300

    Update Design math | Feat

--- projeto-math/styles/script.js
+++ projeto-math/styles/script.js
@@ -1,42 +1,23 @@
-window.addEventListener('scroll', function() {/* section fixada no topo */
+window.addEventListener('scroll', function () {
+  
+  /* section fixada no topo */
   var section = document.querySelector('.fixed-section');
   var sectionOffset = section.offsetTop;
   var scrollPosition = window.scrollY;
-  if (scrollPosition > sectionOffset) {
-      section.classList.add('fixed');
+
+  if (scrollPosition > sectionOffset && this.window.innerWidth > 700) {
+
+    section.classList.add('fixed');
+
   } else {
-      section.classList.remove('fixed');
+
+    section.classList.remove('fixed');
+
   }
+
 });
+
+
 /* Configurações Menu */
-var imgMenu = document.getElementById('img-menu');
-var linkIcon = document.getElementById('link-menu');
-var imgIcon = document.getElementById('casa-icon');
-
-var menu = document.getElementById('menu');
-var allSetas = document.querySelector('.setas');
-var setaOn = document.getElementById('seta01');
-var setaTw = document.getElementById('seta02');
-var setatr = document.getElementById('seta03');
 
 
-
-menu.addEventListener('click', function() {
-
-  if(linkIcon.classList.contains('mostrar')){
-
-    menu.style.transition = 'all 0.6s ease-in-out' 
-    linkIcon.classList.remove('mostrar')
-    imgIcon.classList.remove('mostrar')
-    menu.classList.remove('actived')
-
-  }else{
-
-    linkIcon.classList.add('mostrar')
-    menu.classList.add('actived')
-    menu.style.transition = 'all 0.6s ease-in-out'
-    setTimeout( function() {
-      imgIcon.classList.add('mostrar')
-    }, 400)
-  }
-}) 
